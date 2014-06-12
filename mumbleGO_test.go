5fb2608741a603c283cff14d48d34dba8da173ca
Tobias <tobias.k.schmidt@gmail.com>
2014-06-12 18:46:34 +0200

    working byte conversion

--- mumbleGO_test.go
+++ mumbleGO_test.go
@@ -1,27 +1,15 @@
 package mumbleGO
 
-import "testing"
-import "fmt"
-import "bytes"
+import (
+	"encoding/hex"
+	"fmt"
+	"testing"
+)
 
 func TestBufCreation(t *testing.T) {
-	value := bytes.NewBufferString("00000000000000000003a6K")
-	//var value = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\xa6K"
 	var time uint64 = 239179
-	if x := createBuf(time); x != value {
+	fmt.Println()
+	if x := hex.EncodeToString(createBuf(time).Bytes()); x != "00000000000000000003a64b" {
 		t.Errorf("Bytes are not correct")
-
 	}
-	fmt.Printf("% x", value)
-	fmt.Println("")
-	fmt.Printf("% x", createBuf(time))
-	fmt.Println("")
-
 }
-
-// func TestSqrt(t *testing.T) {
-// 	const in, out = 4, 2
-// 	if x := Sqrt(in); x != out {
-// 		t.Errorf("Sqrt(%v) = %v, want %v", in, x, out)
-// 	}
-// }
